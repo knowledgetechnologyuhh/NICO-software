f263691937a19efee19715e4b39b4adecf073a2b
Marcus Soll <2soll@informatik.uni-hamburg.de>
2016-07-08 16:55:23 +0200

    Fixed setup.py

--- api/motion/setup.py
+++ api/motion/setup.py
@@ -5,7 +5,6 @@ from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
     packages=['motion'],
-    scripts=['srcipts/motion'],
     package_dir={'': 'scripts'}
 )
 
