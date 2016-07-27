17594257726e7b8e415ff78361d6e5954076cf54
Marcus Soll <2soll@informatik.uni-hamburg.de>
2016-07-27 11:49:24 +0200

    Implementation of video device

--- api/src/nicovision/scripts/nicovision/VideoDevice.py
+++ api/src/nicovision/scripts/nicovision/VideoDevice.py
@@ -1,7 +1,9 @@
 import logging
 import cv2
 import os
-import stat
+import inspect
+import threading
+import time
 
 class VideoDevice:
     """
@@ -30,25 +32,9 @@ class VideoDevice:
         return paths
 
     @staticmethod
-    def getAllIDs():
-        """
-        Returns a list of all full IDs of video capturing devices
-
-        :return: IDs of capturing devices
-        :rtype: list
-        """
-        if not os.path.isdir(VideoDevice._VIDEO_DEVICE_PATH):
-            logging.error('Video device path does not exists!')
-            return []
-        ids = []
-        for file in os.listdir(VideoDevice._VIDEO_DEVICE_PATH):
-            ids += [file]
-        return ids
-
-    @staticmethod
     def resolveID(partID):
         """
-        Returns the path of a partial ID
+        Returns the path of a partial string ID
 
         :param partID: Partial ID
         :type partID: str
@@ -68,7 +54,7 @@ class VideoDevice:
             logging.error('No candidates found')
             return ''
         elif len(candidates) is 1:
-            return os.path.realpath(VideoDevice._VIDEO_DEVICE_PATH + candidates[0])
+            return int(os.readlink(VideoDevice._VIDEO_DEVICE_PATH + candidates[0])[-1:])
         else:
             logging.error('Multiple candidates found: {}'.format(candidates))
             return ''
@@ -76,7 +62,7 @@ class VideoDevice:
     @staticmethod
     def fromPartID(partID):
         """
-        Convenience method for creating a VideoDevice from a (partial) ID
+        Convenience method for creating a VideoDevice from a (partial) string ID
 
         :param partID: (Partial) ID
         :type partID: str
@@ -89,31 +75,98 @@ class VideoDevice:
             return None
         return VideoDevice(path)
 
-    def __init__(self, path):
+    def __init__(self, id, framerate=20, width=640, height=480):
         """
         Initialises the VideoDevice. The device starts closed and has to be opened.
 
         If you want to create a VideoDevice from a (partial) ID, use :meth:`nicovision.VideoDevice.fromPartID` instead.
 
-        :param path: Path to device
-        :type path: str
+        :param id: device id
+        :type id: int
         """
         self._capture = None
-        self._path = path
+        self._deviceId = id
         self._valid = True
         self._open = False
-        if not os.lstat(path).st_dev:
-            logging.error('Path %s is not a valid device path')
-            self.valid = False
-        pass
+        self._callback = []
+        self._running = False
+        self._thread = None
+        self._framerate = framerate
+        self._width = width
+        self._height = height
+
+    def setFrameRate(self, framerate):
+        """
+        Sets the current framerate
+
+        :param framerate: Framerate (frames per second)
+        :type framerate: int
+        """
+        self._framerate = framerate
+
+    def getFrameRate(self):
+        """
+        Returns the current frame rate
+
+        :return: Framerate (frames per second)
+        :rtype: int
+        """
+        return self._framerate
+
+    def setResolution(self, width, height):
+        """
+        Sets the current resolution
+
+        :param width: Width
+        :type width: int
+        :param height: Height
+        :type height: int
+        """
+        self._width = width
+        self._height = height
+
+    def getResolution(self):
+        """
+        Returns the current resolution
+
+        :return: (width, height)
+        :rtype: tuple
+        """
+        return (self._width, self._height)
 
     def open(self):
-        # TODO: Implement
-        pass
+        """
+        Opens the device and starts the eventloop
+        """
+        if self._open:
+            logging.warning('Device is already oprn')
+            return
+
+        # Open camera
+        self._capture = cv2.VideoCapture(self._deviceId)
+        self._capture.set(cv2.cv.CV_CAP_PROP_FRAME_WIDTH, self._width)
+        self._capture.set(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT, self._height)
+        self._capture.set(cv2.cv.CV_CAP_PROP_FPS, self._framerate)
+        self._capture.open(self._deviceId)
+
+        # Start eventloop
+        self._running = True
+        self._open = True
+        self._thread = threading.Thread(target=self._eventloop)
+        self._thread.start()
 
     def close(self):
-        # TODO: Implement
-        pass
+        """
+        Stopps the eventloop and closes the device
+        """
+        if not self._open:
+            logging.warning('Trying to close a device which is not open')
+            return
+        self._running = False
+        self._open = False
+        self._thread.join()
+        self._capture.release()
+        self._capture = None
 
     def isOpen(self):
         """
@@ -123,3 +176,38 @@ class VideoDevice:
         :rtype: bool
         """
         return self._open
+
+    def addCallback(self, function):
+        """
+        Adds a callback for the event loop
+
+        Whenever a new frame arrives, all registered callbacks are called.
+
+        The callback must take exactly 2 arguments: rval and frame. frame can be None if any error occures or no image
+        could be grabbed.
+
+        :param function: Function to add as callback
+        :type function: function
+        """
+        if not inspect.isfunction(function):
+            logging.warning('Trying to add non-function callback')
+            return
+        self._callback += [function]
+
+    def cleanCallbacks(self):
+        """
+        Removes all saved callbacks
+        """
+        self._callback = []
+
+    def _eventloop(self):
+        """
+        Internal event loop
+
+        This will call all registered callbacks for each frame
+        """
+        while self._running:
+            rval, frame = self._capture.read()
+            for function in self._callback:
+                function(rval, frame)
+                time.sleep(1.0 / self._framerate)
