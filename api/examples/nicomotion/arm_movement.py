18bca9998560082e1158a504384a09c1387e773e
Marcus Soll <2soll@informatik.uni-hamburg.de>
2016-08-25 10:36:40 +0200

    Added example scripts for motion API

---
+++ api/examples/nicomotion/arm_movement.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import time
+from nicomotion import Motion
+
+time.sleep(5)
+robot = Motion.Motion("../../../json/nico_humanoid_upper_with_hands.json")
+position = 20
+for i in xrange(5):
+	robot.setAngle("r_arm_x", -40 + position, 0.05)
+	robot.setAngle("r_elbow_y", -40 + position, 0.05)
+	if position > 0:
+		print 1
+		robot.closeHand("LHand")
+	else:
+		print 2
+		robot.openHand("LHand")
+	position = position * -1
+        time.sleep(2)
+
+robot.setAngle("r_arm_x", 25, 0.05)
+robot.setAngle("r_elbow_y", 0, 0.05)
+time.sleep(5)
+robot.disableTorqueAll()
