0801ac252380f85b3476e6e6fc9da62ec8b64bb3
Erik Strahl <strahl@informatik.uni-hamburg.de>
2016-10-27 10:54:04 +0200

    Examples finetuned

--- api/examples/nicomotion/arm_movement.py
+++ api/examples/nicomotion/arm_movement.py
@@ -4,7 +4,8 @@ import time
 from nicomotion import Motion
 
 time.sleep(5)
-robot = Motion.Motion("../../../json/nico_humanoid_upper_with_hands.json")
+#robot = Motion.Motion("../../../json/nico_humanoid_upper_with_hands.json",vrep=False)
+robot = Motion.Motion("../../../json/nico_humanoid_upper_with_hands_vrep.json",vrep=True)
 position = 20
 for i in xrange(5):
 	robot.setAngle("r_arm_x", -40 + position, 0.05)
@@ -18,7 +19,7 @@ for i in xrange(5):
 	position = position * -1
         time.sleep(2)
 
-robot.setAngle("r_arm_x", 25, 0.05)
+robot.setAngle("r_arm_x", 20, 0.05)
 robot.setAngle("r_elbow_y", 0, 0.05)
 time.sleep(5)
 robot.disableTorqueAll()
