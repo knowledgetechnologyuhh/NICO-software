ddb627e4276d8522d274be6daaf96fa298e7b998
4gaede <4gaede@informatik.uni-hamburg.de>
2017-07-21 16:00:15 +0200

    renamed test.html->home.html

--- api-doc/conf.py
+++ api-doc/conf.py
@@ -148,7 +148,7 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-html_sidebars = { '**': ['test.html', 'globaltoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html'], }
+html_sidebars = { '**': ['home.html', 'globaltoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html'], }
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
