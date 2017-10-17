905cf82b29ad4836901400d3101ed0b96456426e
strahl <strahl@informatik.uni-hamburg.de>
2017-10-17 15:37:47 +0200

    scripts for touch sensor experiment added

---
+++ api/examples/mixed-modules/touch-sensor-experiments/create_experiment_db.py
@@ -0,0 +1,42 @@
+import sqlite3
+connection = sqlite3.connect("./data/experiment.db")
+
+cursor = connection.cursor()
+
+# delete
+#cursor.execute("""DROP TABLE sample;""")
+#cursor.execute("""DROP TABLE sub_sample;""")
+
+sql_command = """
+CREATE TABLE sample ( 
+sample_number INTEGER PRIMARY KEY, 
+object_name VARCHAR(20), 
+timecode VARCHAR(30));"""
+
+cursor.execute(sql_command)
+
+sql_command = """
+CREATE TABLE subsample ( 
+subsample_number INTEGER PRIMARY KEY,
+sample_number INTEGER,
+subsample_iteration INTEGER,
+thumb_position FLOAT,
+finger_position FLOAT,
+thumb_current FLOAT,
+finger_current FLOAT,
+timecode_local VARCHAR(30),
+status INTEGER,
+status_touch INTEGER,
+counter_touch INTEGER,
+x_touch INTEGER,
+y_touch INTEGER,
+z_touch INTEGER,
+timecode_touch VARCHAR(30));"""
+
+cursor.execute(sql_command)
+
+# never forget this, if you want the changes to be saved:
+connection.commit()
+
+connection.close()
+
