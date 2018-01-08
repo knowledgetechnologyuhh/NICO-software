1152c8dda674230842aab7f2f4c4a59449d0a986
4gaede <4gaede@informatik.uni-hamburg.de>
2018-01-08 16:17:22 +0100

    added functionality to allow the robot to mirror the detected emotion and updated example

--- api/examples/nicoemotionrecognition/EmotionRecognitionExample.py
+++ api/examples/nicoemotionrecognition/EmotionRecognitionExample.py
@@ -2,8 +2,8 @@ import time
 from nicoemotionrecognition import EmotionRecognition
 
 emotionRecogniton = EmotionRecognition.EmotionRecognition()
-emotionRecogniton.start(showGUI=True)
-time.sleep(10)
+emotionRecogniton.start(showGUI=True, mirrorEmotion=True)
+raw_input("Press enter to stop\n")
 print emotionRecogniton.getDimensionalData()
 print emotionRecogniton.getCategoricalData()
 
