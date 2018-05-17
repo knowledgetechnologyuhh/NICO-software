fb99caaef82b8e28ea13bf11b664a0486ff23157
strahl <strahl@informatik.uni-hamburg.de>
2018-05-17 17:04:09 +0200

    ImageRecorderMultipleExample improved. Works for two cameras now and takes command line arguments.

--- api/examples/nicovision/ImageRecorderMultipleExample.py
+++ api/examples/nicovision/ImageRecorderMultipleExample.py
@@ -4,6 +4,8 @@ import os
 from os.path import dirname, abspath
 from time import sleep
 import datetime
+import sys
+
 
 try:
     os.mkdir(dirname(abspath(__file__))+'/recorded_images')
@@ -15,25 +17,43 @@ try:
 except OSError:
     pass
 
-# try:
-#     os.mkdir(dirname(abspath(__file__))+'/recorded_images/camera2')
-# except OSError:
-#     pass
+try:
+    os.mkdir(dirname(abspath(__file__))+'/recorded_images/camera2')
+except OSError:
+    pass
 
-logging.getLogger().setLevel(logging.INFO)
-device = ImageRecorder.get_devices()[0]
-ir = ImageRecorder.ImageRecorder(device, 1920, 1080)
-# device2 = ImageRecorder.get_devices()[1]
-# ir2 = ImageRecorder.ImageRecorder(device2, 640, 480)
-sleep(1)
-print("Start taking pictures")
-print(datetime.datetime.today().isoformat())
-ir.start_recording(
-    path=dirname(abspath(__file__))+'/recorded_images/camera1/picture-{}.png')
-# ir2.start_recording(
-#     path=dirname(abspath(__file__))+'/recorded_images/camera2/picture-{}.png')
-sleep(3)
-print("Stop recording")
-ir.stop_recording()
-# ir2.stop_recording()
-print("Finished taking pictures")
+print len(sys.argv)
+if len(sys.argv)!=5:
+	print "please call me with the parameters"
+	print "resolution_x, resolution_y, framerate, number_of_cameras_to_use"
+else:
+	res_x=int(sys.argv[1])
+	res_y=int(sys.argv[2])
+	framerate=int(sys.argv[3])
+	amount_of_cams=int(sys.argv[4])
+	logging.getLogger().setLevel(logging.INFO)
+	print "devices" + str( ImageRecorder.get_devices() )
+	device = ImageRecorder.get_devices()[0]
+	ir = ImageRecorder.ImageRecorder(device, res_x, res_y,framerate=framerate,writer_threads=5)
+	#ir = ImageRecorder.ImageRecorder(device, 1920, 1080,framerate=30,writer_threads=5)
+	#ir = ImageRecorder.ImageRecorder(device, 3840, 2160,framerate=30,writer_threads=5)
+	#ir = ImageRecorder.ImageRecorder(device, 1280, 720,framerate=20,writer_threads=5)
+	if amount_of_cams>=2:
+		device2 = ImageRecorder.get_devices()[1]
+		ir2 = ImageRecorder.ImageRecorder(device2, res_x, res_y,framerate=framerate,writer_threads=5)
+	#ir2 = ImageRecorder.ImageRecorder(device2, 1920, 1080,framerate=30,writer_threads=5)
+	#ir2 = ImageRecorder.ImageRecorder(device2, 3840, 2160,framerate=30,writer_threads=5)
+	sleep(2)
+	print("Start taking pictures")
+	print(datetime.datetime.today().isoformat())
+	ir.start_recording(
+		path=dirname(abspath(__file__))+'/recorded_images/camera1/picture-{}.png')
+	if amount_of_cams>=2:
+		ir2.start_recording(
+			 path=dirname(abspath(__file__))+'/recorded_images/camera2/picture-{}.png')
+	sleep(10)
+	print("Stop recording")
+	ir.stop_recording()
+	if amount_of_cams>=2:
+		ir2.stop_recording()
+	print("Finished taking pictures")
