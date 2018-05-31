040abc1318f9ad813724ec8db232dcbbee4411a2
strahl <strahl@informatik.uni-hamburg.de>
2018-05-31 12:20:31 +0200

    tidied up nicovision examples

--- api/examples/nicovision/test.sh
+++ api/examples/nicovision/test.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 ./del.sh
-python ImageRecorderMultipleExample_multimodal_writer.py 1920 1080 30 2
+python ImageRecorderMultipleExample_customized_writer.py 1920 1080 30 2
 ls -1 ./recorded_images/camera1/ | wc -l
-wc -l ./left_cam_synced_data.csv
 ls -1 ./recorded_images/camera2/ | wc -l
-wc -l ./right_cam_synced_data.csv
