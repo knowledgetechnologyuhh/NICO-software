332cdb94adf1dcc558e7307ffc3900162646e58c
strahl <strahl@informatik.uni-hamburg.de>
2018-05-31 15:55:28 +0200

    improvement in multimodal test

--- api/examples/multimodal_tests/test_with_sound.sh
+++ api/examples/multimodal_tests/test_with_sound.sh
@@ -9,6 +9,7 @@ python multimodal_writer_vision_joints_sound.py 1920 1080 30 2
 RCP1=$(ls -1 ./recorded_images/camera1/ | wc -l)
 DL1=$(wc -l ./left_cam_synced_data.csv | awk '{print $1}' )
 DL=$(expr $DL1 - 1 )
+printf "\n\nWe ran a test for 5s at 30fps. So we exspect at about 150 pictures, 150 datalines and 5s of sound recording.\n\n"
 printf "\nPics from cam1: %s Datalines: %s\n" "$RCP1" "$DL"
 RCP2=$(ls -1 ./recorded_images/camera2/ | wc -l)
 DL2=$(wc -l ./right_cam_synced_data.csv | awk '{print $1}' )
