afed466fc605519fdf6a0bc682e38c5b84a9346a
4gaede <4gaede@informatik.uni-hamburg.de>
2018-06-20 16:44:18 +0200

    added hardware zoom, pan and tilt to nicovision

--- api/examples/nicovision/VideoRecorderExample.py
+++ api/examples/nicovision/VideoRecorderExample.py
@@ -7,7 +7,8 @@ from nicovision import VideoRecorder
 recording_time = 15
 
 print('Starting a recording for %i seconds' % recording_time)
-r = VideoRecorder.VideoRecorder(device="usb-046d_080a_6C686AA1-video-index0", width=1920, height=1080, framerate=30,
+r = VideoRecorder.VideoRecorder(device="usb-046d_080a_6C686AA1-video-index0",
+                                width=1920, height=1080, framerate=30,
                                 videoformat=VideoRecorder.VideoCodec.XVID)
 
 r.start_recording('./', 'VideoRecorderExample')
--- api/examples/nicovision/framerate_test.bash
+++ api/examples/nicovision/framerate_test.bash
@@ -4,7 +4,7 @@
 rm recorded_images/camera0/*.png
 rm recorded_images/camera1/*.png
 # resolution_x, resolution_y, framerate, number_of_cameras_to_use
-python ImageRecorderMultipleExample.py 640 480 15 2
+python ImageRecorderMultipleExample.py 1920 1080 10 2 400
 # count number of recorded images
 echo "Number of images recorded by camera 0:"
 find recorded_images/camera0 -type f | wc -l
