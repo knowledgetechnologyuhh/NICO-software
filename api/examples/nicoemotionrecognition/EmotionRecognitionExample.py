3a4c764d5acf4c0754539737327942a27f90dd64
Erik Strahl <strahl@informatik.uni-hamburg.de>
2018-09-17 12:40:51 +0200

    adapted Emotion Recognition Demo to new Video Device Interface

--- api/examples/nicoemotionrecognition/EmotionRecognitionExample.py
+++ api/examples/nicoemotionrecognition/EmotionRecognitionExample.py
@@ -5,12 +5,13 @@ from nicoface.FaceExpression import faceExpression
 from os.path import dirname, abspath
 
 robot = Motion.Motion(dirname(abspath(__file__))+"/../../../json/nico_humanoid_upper.json",vrep=False)
-face = faceExpression("/dev/ttyACM0")
+face = faceExpression()
 #torso NICO
-#emotionRecogniton = EmotionRecognition.EmotionRecognition(device='usb-046d_080a_6C686AA1-video-index0', robot=robot, face=face)
+
+emotionRecogniton = EmotionRecognition.EmotionRecognition(device='usb-046d_080a_6C686AA1-video-index0', robot=robot, face=face)
 #legged NICO
 
-emotionRecogniton = EmotionRecognition.EmotionRecognition(device='usb-046d_080a_17E79161-video-index0', robot=robot, face=face,faceDetectionDelta=10, voiceEnabled=True)
+#emotionRecogniton = EmotionRecognition.EmotionRecognition(device='usb-046d_080a_17E79161-video-index0', robot=robot, face=face,faceDetectionDelta=10, voiceEnabled=True)
 
 #emotionRecogniton = EmotionRecognition.EmotionRecognition(device='usb-046d_080a_2DE7B460-video-index0', face=face)
 
