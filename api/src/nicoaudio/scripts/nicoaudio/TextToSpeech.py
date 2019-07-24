8f6ba9ab7f3d311bf2513ba7999e73627f9f971e
4gaede <4gaede@informatik.uni-hamburg.de>
2019-07-24 14:13:30 +0200

    updated caching of TTS to allow same sentence with different voices/languages

--- api/src/nicoaudio/scripts/nicoaudio/TextToSpeech.py
+++ api/src/nicoaudio/scripts/nicoaudio/TextToSpeech.py
@@ -4,7 +4,9 @@ import logging
 import os
 import subprocess
 import time
+from io import BytesIO
 
+import requests
 from gtts import gTTS, lang
 from nicoaudio.AudioPlayer import AudioPlayer
 
@@ -16,31 +18,35 @@ class TextToSpeech(object):
     TextToSpeech is a simple text to speech module for different languages
     """
 
-    def __init__(self, cache_dir="/tmp"):
+    def __init__(self, cache_dir="/tmp", port="http://0.0.0.0:5002/api/tts"):
         """
-        TextToSpeech allows simple text to speech using gTTS if google is
-        available or pico2wave as fallback if there is no cached file.
+        TextToSpeech allows simple text to speech using locally hosted mozilla
+        tts (https://github.com/mozilla/TTS), gTTS if google is available or
+        pico2wave as fallback if there is no cached file.
 
         :param cache_dir: directory where generated audiofiles should be
                           stored.
         :type cache_dir: str
+        :param port: port at which mozilla tts is hosted (uses fallbacks if
+                     connection fails)
+        :type port: str
         """
         # load cache dictionary
         if not os.path.isdir(cache_dir):
-            logger.error(
-                "Cache directory '{}' does not exist.".format(cache_dir))
+            logger.error("Cache directory '%s' does not exist.", cache_dir)
             raise IOError("Directory '{}' does not exist.".format(cache_dir))
         cache_file = "{}/NICO_speech_cache.json".format(cache_dir)
         if os.path.isfile(cache_file):
-            logger.info("Loading '{}'".format(cache_file))
+            logger.info("Loading '%s'", cache_file)
             with open(cache_file) as f:
                 self._cache = json.load(f)
         else:
-            logger.info("File {} not found - initializing empty "
-                        "cache".format(cache_file))
+            logger.info("File %s not found - initializing empty cache",
+                        cache_file)
             self._cache = {}
         self._cache_dir = cache_dir
         self._cache_file = cache_file
+        self._port = port
 
     def say(self, text, language="en-GB", pitch=0, speed=1., blocking=True):
         """
@@ -48,7 +54,7 @@ class TextToSpeech(object):
         pico2wave as fallback if there is no cached file available.
 
         :param text: text to speak
-        :type filename: str
+        :type text: str
         :param language: Language code (e.g. 'en-GB' or 'de')
         :type language: str
         :param pitch: Pitch in octaves by which to shift the output
@@ -63,11 +69,25 @@ class TextToSpeech(object):
         :return: Playback duration left (0 if blocking)
         :rtype: float
         """
-        if text in self._cache and False:
+        # try to use mozilla tts server
+        tts_server_online = False
+        if language.startswith("en"):
+            try:
+                response = requests.get(self._port, params={"text": text})
+                tts_server_online = True
+            except requests.exceptions.ConnectionError:
+                logger.warn("Failed to connect to port %s", self._port)
+
+        if (tts_server_online and
+                response.headers['Content-Type'] == 'audio/wav'):
+            # play server response if online
+            file = BytesIO(response.content)
+            playback = AudioPlayer(file)
+        elif text + language in self._cache:
             # load cached file if it exists
-            filename = self._cache[text]
-            logger.info("Using cached audio file {}".format(filename))
-            playback = AudioPlayer(filename)
+            file = self._cache[text + language]
+            logger.info("Using cached audio file %s", file)
+            playback = AudioPlayer(file)
         elif os.system("ping -c 1 google.com > /dev/null 2>&1") == 0:
             # use gTTS if google is available
 
@@ -86,13 +106,13 @@ class TextToSpeech(object):
 
             # generate audio file
             tts = gTTS(text, language, slow=False)
-            filename = '/tmp/NICO_speech_{}.mp3'.format(
+            file = '/tmp/NICO_speech_{}.mp3'.format(
                 datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S.%f"))
-            tts.save(filename)
-            playback = AudioPlayer(filename)
+            tts.save(file)
+            playback = AudioPlayer(file)
 
             # cache audio file
-            self._cache[text] = filename
+            self._cache[text + language] = file
             with open(self._cache_file, "wb") as f:
                 f.write(json.dumps(self._cache))
         else:
@@ -103,18 +123,18 @@ class TextToSpeech(object):
             if language == "de":
                 language = "de-DE"
             try:
-                filename = '/tmp/NICO_speech_{}.wav'.format(
+                file = '/tmp/NICO_speech_{}.wav'.format(
                     datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S.%f"))
                 subprocess.call(["pico2wave", "-l", language,
-                                 "-w", filename, text])
-                playback = AudioPlayer(filename)
-                os.remove(filename)
-            except OSError as e:
+                                 "-w", file, text])
+                playback = AudioPlayer(file)
+                os.remove(file)
+            except OSError:
                 logger.error(
                     "Could not execute pico2wave - make sure "
                     "'libttspico-utils' apt package is installed")
                 raise
-            except IOError as e:
+            except IOError:
                 logger.error(
                     "Language '{}' is not supported by pico2wave".format(
                         language))
