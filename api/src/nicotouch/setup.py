82071675519647e546da2b456649d8fe8fed2504
4gaede <4gaede@informatik.uni-hamburg.de>
2019-09-19 14:54:41 +0200

    Python 3 compatibility for nicotouch

--- api/src/nicotouch/setup.py
+++ api/src/nicotouch/setup.py
@@ -2,6 +2,10 @@
 
 from setuptools import find_packages, setup
 
+extra = {}
+if sys.version_info >= (3,):
+    extra['use_2to3'] = True
+
 setup(
     name="nicotouch",
     version="1.0",
@@ -10,4 +14,5 @@ setup(
     description='NICO api package to access touch sensors',
     author='Connor Gaede',
     author_email='4gaede@informatik.uni-hamburg.de',
+    **extra
 )
