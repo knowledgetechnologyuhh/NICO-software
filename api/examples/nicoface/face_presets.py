29b346f81cab80bfaa455356af92a3ae734f04ac
4gaede <4gaede@informatik.uni-hamburg.de>
2019-10-23 14:40:11 +0200

    refactored nicoface to improve performance

--- api/examples/nicoface/face_presets.py
+++ api/examples/nicoface/face_presets.py
@@ -2,20 +2,28 @@ from time import sleep
 
 from nicoface.FaceExpression import faceExpression
 
-simulation = False
+simulation = True
 if simulation:
-    fe = faceExpression('sim')
+    fe = faceExpression(simulation=True)
 else:
     fe = faceExpression()
     # handcoded presets
     # 'happiness','sadness','anger','disgust','surprise','fear','neutral','clear'
-    for expression in ('happiness', 'sadness', 'anger', 'disgust', 'surprise',
-                       'fear', 'neutral', 'clear'):
+    for expression in (
+        "happiness",
+        "sadness",
+        "anger",
+        "disgust",
+        "surprise",
+        "fear",
+        "neutral",
+        "clear",
+    ):
         fe.sendFaceExpression(expression)  # only works with a real robot
         sleep(1)
 
 # trained expressions
-for expression in ('Angry', 'Happy', 'Neutral', 'Sad', 'Surprise'):
+for expression in ("Angry", "Happy", "Neutral", "Sad", "Surprise"):
     print(expression)
     fe.sendTrainedFaceExpression(expression)
     sleep(1)
