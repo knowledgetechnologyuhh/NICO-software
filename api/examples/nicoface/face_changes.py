31e779367f4cff1a0041380426ff61a66875c30b
4gaede <4gaede@informatik.uni-hamburg.de>
2019-10-29 16:23:09 +0100

    updated nicoface
    fixed autodetection issues due to communication delay
    improved logging
    unified names for trained and hand designed presets
    adjusted examples accordingly

--- api/examples/nicoface/face_changes.py
+++ api/examples/nicoface/face_changes.py
@@ -4,16 +4,18 @@
 #
 from nicoface.FaceExpression import faceExpression
 import time
-import timeit
+import logging
 
-#Change your interface here
+logging.basicConfig(level=logging.INFO)
+
+# Change your interface here
 fe = faceExpression()
 
 # Chnange betwee two faces
 
-for i in range(2,-1,-1):
+for i in range(2, -1, -1):
     fe.setCommMode(i)
-    #Generate the mouth form and eyebrowse
+    # Generate the mouth form and eyebrowse
     # Using the standard values
     fe.mouth = fe.gen_mouth()
     fe.left = fe.gen_eyebrowse(type="l")
@@ -22,20 +24,19 @@ for i in range(2,-1,-1):
 
     time.sleep(0.05)
 
-
-    #Using both tuples for two mouth lines
-    fe.mouth = fe.gen_mouth((1.0,-0.8,1.0,0),(None,None,None,None))
-    fe.left = fe.gen_eyebrowse((-0.9,0,1,0),type="l")
-    fe.right = fe.gen_eyebrowse((-0.9,0,1,0),type="r")
+    # Using both tuples for two mouth lines
+    fe.mouth = fe.gen_mouth((1.0, -0.8, 1.0, 0), (None, None, None, None))
+    fe.left = fe.gen_eyebrowse((-0.9, 0, 1, 0), type="l")
+    fe.right = fe.gen_eyebrowse((-0.9, 0, 1, 0), type="r")
 
     fe.send()
 
     time.sleep(0.05)
 
-for i in range (0,12,2):
-    w1=1.0-0.24*i
+for i in range(0, 12, 2):
+    w1 = 1.0 - 0.24 * i
     w2 = -0.8 + 0.15 * i
-    #w2=0.2
+    # w2=0.2
     fe.mouth = fe.gen_mouth((w1, w2, 1.0, 0), (None, None, None, None))
     fe.send("m")
     time.sleep(0.001)
--- api/examples/nicoface/face_expressions_learned_and_designed.py
+++ api/examples/nicoface/face_expressions_learned_and_designed.py
@@ -13,27 +13,20 @@ fe = faceExpression()
 sleep(1)
 
 # trained expressions
-for expression in (
-    ("Angry", "anger"),
-    ("Happy", "happiness"),
-    ("Neutral", "neutral"),
-    ("Sad", "sadness"),
-    ("Surprise", "surprise"),
-):
-    texpress, dexpress = expression
-    print("Showing " + dexpress)
+for expression in (("anger"), ("happiness"), ("neutral"), ("sadness"), ("surprise")):
+    print("Showing " + expression)
     key = "b"
     face_choice = choice([True, False])
     while key == "b":
         if face_choice:
             print("Trained first")
-            fe.sendTrainedFaceExpression(texpress)
+            fe.sendTrainedFaceExpression(expression)
             raw_input()
-            fe.sendFaceExpression(dexpress)
+            fe.sendFaceExpression(expression)
         else:
             print("Designed first")
-            fe.sendFaceExpression(dexpress)
+            fe.sendFaceExpression(expression)
             key = raw_input()
-            fe.sendTrainedFaceExpression(texpress)
+            fe.sendTrainedFaceExpression(expression)
         key = raw_input()
         # sleep(1)
