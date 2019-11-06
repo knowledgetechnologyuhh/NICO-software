96fa94186ccc321480a77c7f3784d36e729a7135
4gaede <4gaede@informatik.uni-hamburg.de>
2019-11-06 18:14:10 +0100

    Improved NICO-python3.bash and NICO-setup.bash
    -Setup now generates 'activate.bash' to source virtualenv and catkin
    workspaces
    -Added warnings and error messages
    -Failures at critical points will now cause the installation to stop
    -PyRep related environment variable VREP_ROOT is now checked before
    pypot installation and can also be ignored if necessary

--- api/src/nicomotion/scripts/nicomotion/__init__.py
+++ api/src/nicomotion/scripts/nicomotion/__init__.py
@@ -1,9 +1,10 @@
 import sys
+import os
 
 try:
     from pyrep import PyRep  # required to prevent vrep launch issues
 except (ImportError, SyntaxError):
-    if sys.version_info < (3,):
+    if sys.version_info < (3,) or "VREP_ROOT" not in os.environ:
         pass  # pyrep is not Python 2 compatible
     else:
         raise
